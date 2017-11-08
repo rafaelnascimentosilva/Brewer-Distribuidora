61ae83e21f5dc74089197dbcc61945f23c51a92d
Rafael_user <rafaelsilva@lme.ufc.br>
2017-11-08 20:09:30 -0300

    criando cadastro de cliente

---
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -0,0 +1,12 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientesController {
+	@RequestMapping("cliente/novo")
+	public String novo() {
+		return "cliente/CadastroCliente";
+	}
+}
