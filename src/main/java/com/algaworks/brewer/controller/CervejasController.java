e400b3bd0d2008395d35b291464e0af1eda0d73e
rafaelnascimentosilva <rafaelnasc.silv@gmail.com>
2018-02-27 23:09:27 -0300

    adicionando migrations, configurando projeto com spring data jpa

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
 
+	@Autowired
+	private Cervejas cervejas;
+	
 	@RequestMapping("cerveja/novo")
 	public String novo() {
+		cervejas.findAll();//apagar..
 		return "cerveja/CadastroCerveja";	
 	}
 
